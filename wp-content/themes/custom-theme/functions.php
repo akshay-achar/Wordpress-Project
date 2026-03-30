4284793353ba04a5321ed0c7cfeadfa380a9b6d5
AKSHAY K <akshayachar2011@gmail.com>
2026-03-30 17:04:38 +0530

    Created a custom theme

---
+++ wp-content/themes/custom-theme/functions.php
@@ -0,0 +1,5 @@
+<?php
+function my_theme_setup() {
+    add_theme_support('title-tag');
+}
+add_action('after_setup_theme', 'my_theme_setup');
---
+++ wp-content/themes/custom-theme/index.php
@@ -0,0 +1,7 @@
+<?php
+// echo "Hello from Custom theme";
+get_header(); ?>
+
+<h1>Welcome</h1>
+
+<?php get_footer(); ?>
