0bc6a97aa5520fc364395634821d539ab45a5fbd
AKSHAY K <akshayachar2011@gmail.com>
2026-03-30 16:16:45 +0530

    Added wp filter hooks

--- wp-content/plugins/test-plugin/create.php
+++ wp-content/plugins/test-plugin/create.php
@@ -9,3 +9,7 @@ Version: 1.0
 add_action('wp_footer', function() { 
     echo '<h1>Hello from the test plugin </h1>';
 });
+
+add_filter('the_content', function($content) {
+    return "<p>Start</p>" . $content . "<p>End</p>";
+});
